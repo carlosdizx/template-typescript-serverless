be6653d7fd84655a66311f00cb71989984cd181c
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-06 12:24:38 -0500

    :sparkles: installed pg typeorm reflect-metadata for connect to postgres and configure for connection in local

---
+++ src/utils/DatabaseConnection.ts
@@ -0,0 +1,22 @@
+import { DataSource } from "typeorm";
+import dotenv = require("dotenv");
+dotenv.config();
+
+const dataSource =  new DataSource({
+    type: 'postgres',
+    host: process.env.DB_POSTGRES_HOST,
+    port: parseInt(`${process.env.DB_POSTGRES_PORT}`),
+    username: process.env.DB_POSTGRES_USER,
+    password: process.env.DB_POSTGRES_PASSWORD,
+    database: process.env.DB_POSTGRES_DATABASE,
+})
+
+const getConnect = async () => {
+    if(!dataSource.isInitialized) {
+        console.log("Connected to database");
+        await  dataSource.initialize();
+    }else console.log("recycled connection");
+
+}
+
+export default getConnect;
